6ee37e98c11f882b48d9d28d1be036a614487e76
Fede P53s <feede.7@gmail.com>
2024-03-05 22:07:29 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+function sleep (milliseconds) {
+  const start = new Date().getTime()
+  for (let i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break
+    }
+  }
+}
+
+const flowLoopback = addKeyword(['hola123'])
+  .addAnswer(
+    [
+      'Bueno, ahí va, agarrate 🫠'
+    ],
+    null,
+    async (ctx, { flowDynamic }) => {
+      const msgSplit = ctx.body.split(' ')
+      let start = 0
+      let delay = 0
+      console.log(`mensaje: ${ctx.body}`)
+
+      if (~isNaN(parseInt(msgSplit[1]))) {
+        start = 2
+        delay = parseInt(msgSplit[1])
+      } else {
+        start = 1
+        delay = 1000
+      }
+
+      for (let i = start; i < msgSplit.length; i++) {
+        console.log(`${msgSplit[i]}`)
+        await flowDynamic([{ body: msgSplit[i], delay }])
+      }
+    }
+  )
+
+const main = async () => {
+  const adapterDB = new MockAdapter()
+  const adapterFlow = createFlow([flowLoopback])
+  const adapterProvider = createProvider(BaileysProvider)
+
+  createBot({
+    flow: adapterFlow,
+    provider: adapterProvider,
+    database: adapterDB
+  })
+
+  const HOLA = 'pedo'
+  console.log(`que onda ${HOLA.replace('ped', 'cac')}`)
+
+  const msgSplit = 'habia una vez un circo'.split(' ')
+  for (let i = 1; i < msgSplit.length; i++) {
+    console.log(`${msgSplit[i]}`)
+    sleep(500)
+  }
+
+  for (const msg of msgSplit) {
+    if (msg !== 'habia') {
+      console.log(`${msg}`)
+      sleep(500)
+    }
+  }
+
+  QRPortalWeb()
+}
+
+main()
